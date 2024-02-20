ff993c9fb27302d846e6d7ade9160d2335e14a79
iusui <160591376+iusui@users.noreply.github.com>
2024-02-20 18:59:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 
 
   window.botpressWebChat.init({
-      "composerPlaceholder": "Chat with bot",
+      "composerPlaceholder": "Chat with Bot",
       "botConversationDescription": "Cinefreak",
       "botId": "915e55d7-e3ea-4e77-b501-b075167c2177",
       "hostUrl": "https://cdn.botpress.cloud/webchat/v1",
@@ -13,7 +13,7 @@
       "lazySocket": true,
       "themeName": "prism",
       "botName": "Estrella Bot",
-      //"avatarUrl": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.techopedia.com%2Fdefinition%2F24063%2Finternet-bot&psig=AOvVaw0FO-VGG4CLgjzUyaFqBIFF&ust=1708514598921000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCLilkoDnuYQDFQAAAAAdAAAAABAJ",
+      "stylesheet": "https://webchat-styler-css.botpress.app/prod/code/ba07d714-9f17-45f6-8289-c95dae10cede/v19983/style.css",
       "frontendVersion": "v1",
       "useSessionStorage": true,
       "theme": "prism",
