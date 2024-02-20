f7ba904c145d6224d9dc9aa3dca0133216d19285
iusui <160591376+iusui@users.noreply.github.com>
2024-02-20 15:43:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,21 @@
 // 20240220132444
 // https://mediafiles.botpress.cloud/915e55d7-e3ea-4e77-b501-b075167c2177/webchat/config.js
 
-window.botpressWebChat.init({
-    "composerPlaceholder": "Chat with bot",
-    "botConversationDescription": "Cinephile bot",
-    "botId": "915e55d7-e3ea-4e77-b501-b075167c2177",
-    "hostUrl": "https://cdn.botpress.cloud/webchat/v1",
-    "messagingUrl": "https://messaging.botpress.cloud",
-    "clientId": "915e55d7-e3ea-4e77-b501-b075167c2177",
-    "webhookId": "dc360eb7-1821-4e41-aa93-44fbee371f6c",
-    "lazySocket": true,
-    "themeName": "prism",
-    "frontendVersion": "v1",
-    "useSessionStorage": true,
-    "theme": "prism",
-    "themeColor": "#2563eb"
+
+  window.botpressWebChat.init({
+      "composerPlaceholder": "Chat with Daddy",
+      "botConversationDescription": "This chatbot was built surprisingly fast with Botpress",
+      "botId": "915e55d7-e3ea-4e77-b501-b075167c2177",
+      "hostUrl": "https://cdn.botpress.cloud/webchat/v1",
+      "messagingUrl": "https://messaging.botpress.cloud",
+      "clientId": "915e55d7-e3ea-4e77-b501-b075167c2177",
+      "webhookId": "dc360eb7-1821-4e41-aa93-44fbee371f6c",
+      "lazySocket": true,
+      "themeName": "prism",
+      "botName": "Daddy",
+      "avatarUrl": "https://scontent.fccu3-1.fna.fbcdn.net/v/t39.30808-6/266289338_3198689887124845_670228095507260885_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=9c7eae&_nc_ohc=SL6I0XOLEIwAX-Si-h-&_nc_ht=scontent.fccu3-1.fna&oh=00_AfAm5ts89m7qCnDuW62LYsw6PiwgwWy4FR-wTSMcaC9iCA&oe=65D9E29E",
+      "frontendVersion": "v1",
+      "useSessionStorage": true,
+      "theme": "prism",
+      "themeColor": "#2563eb"
   });
