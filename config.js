91c701b2606b472517189608d5fe285aa098af4d
iusui <160591376+iusui@users.noreply.github.com>
2024-02-20 14:58:51 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+// 20240220132444
+// https://mediafiles.botpress.cloud/915e55d7-e3ea-4e77-b501-b075167c2177/webchat/config.js
+
+window.botpressWebChat.init({
+    "composerPlaceholder": "Chat with bot",
+    "botConversationDescription": "Cinephile bot",
+    "botId": "915e55d7-e3ea-4e77-b501-b075167c2177",
+    "hostUrl": "https://cdn.botpress.cloud/webchat/v1",
+    "messagingUrl": "https://messaging.botpress.cloud",
+    "clientId": "915e55d7-e3ea-4e77-b501-b075167c2177",
+    "webhookId": "dc360eb7-1821-4e41-aa93-44fbee371f6c",
+    "lazySocket": true,
+    "themeName": "prism",
+    "frontendVersion": "v1",
+    "useSessionStorage": true,
+    "theme": "prism",
+    "themeColor": "#2563eb"
+  });
